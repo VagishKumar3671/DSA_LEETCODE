63ca94a2e854c22e49bf1709c434565f5647db4d
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-02 01:00:27 +0530

    Time: 3 ms (98.82%), Space: 12.6 MB (68.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,11 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if sorted(s)==sorted(t):
-            return True
-        else:
+        if len(s)!=len(t):
             return False
-        
+        else:
+            for i in set(s):
+                if s.count(i)!=t.count(i):
+                    return False
+                    break
+            else:
+                return True
