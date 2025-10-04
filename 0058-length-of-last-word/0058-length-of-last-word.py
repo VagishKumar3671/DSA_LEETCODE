83c4a6427321a4f1c167a4aeb16fa0d4222b899d
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-05 03:13:51 +0530

    Time: 0 ms (100%), Space: 12.5 MB (45.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,5 +1,5 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        if len(s)==0:
-            return 0
+        if len(s)==0 or len(s)==1:
+            return len(s)
         return len(s.split()[-1])
