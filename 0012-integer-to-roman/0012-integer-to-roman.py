5e0beddf78f94c889762b936d4ec46652a8d501a
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-06 01:53:40 +0530

    Time: 0 ms (100%), Space: 12.4 MB (83.32%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def intToRoman(self, num):
-        vals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        syms = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        res = ''
+        vals,syms,res = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"],''
         for i in range(len(vals)):
             res += (num // vals[i]) * syms[i]
             num %= vals[i]
