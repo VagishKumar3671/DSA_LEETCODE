f8e293020fe95ad9afe346a144c5ba6f85a14a85
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-08 15:24:11 +0530

    Time: 0 ms (100%), Space: 12.6 MB (12.78%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def combine(self, temp1, temp2):
+        temp = []
+        for i in temp1:
+            for j in temp2:
+                temp.append(i + j)
+        return temp
+    def letterCombinations(self, digits):
+        if len(digits) == 0:
+            return []
+        dic = {
+            '0': [''],
+            '1': [''],
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        if len(digits) == 1:
+            return dic[digits[0]]
+        temp1 = dic[digits[0]]
+        for i in range(1, len(digits)):
+            temp2 = dic[digits[i]]
+            temp1 = self.combine(temp1, temp2)
+        return temp1
