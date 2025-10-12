e4f1c9757edc68f392917e02e95e6d3c5c6f962a
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-13 01:12:49 +0530

    Time: 0 ms (100%), Space: 13 MB (58.08%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,20 +1,15 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        # Case 1: Only one element
         if len(nums) == 1:
             if nums[0] >= target:
                 return 0
             return 1
-
-        # Case 2: Target is greater than or equal to last element
         if target > nums[-1]:
             return len(nums)
-
-        # Case 3: Target is smaller than the first element
+        if target == nums[-1]:
+            return len(nums) - 1
         if target <= nums[0]:
             return 0
-
-        # Case 4: Binary Search for correct position
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
