eefe0ad15fb485c43c981e907b5a30b017fab775
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-15 23:55:43 +0530

    Time: 15 ms (34.72%), Space: 12.5 MB (52.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,2 +1,13 @@
-class Solution(object):
-    def isPalindrome(self, x): return str(x) == str(x)[::-1]
+class Solution:
+    def isPalindrome(self, x):
+        # Negative numbers and numbers ending with 0 (except 0 itself) are not palindrome
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_num = 0
+        temp = x
+        while temp > 0:
+            reversed_num = reversed_num * 10 + temp % 10
+            temp //= 10
+
+        return x == reversed_num
