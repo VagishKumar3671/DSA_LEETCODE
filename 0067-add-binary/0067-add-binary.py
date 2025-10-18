88727597f5a9d2f5ee9b58798efe161476d0f4fa
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-19 00:57:58 +0530

    Time: 4 ms (49.49%), Space: 12.5 MB (14.52%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -4,18 +4,10 @@ class Solution(object):
         a,b = ('0' * ((length_max + 1) - len(a))) + a,('0' * ((length_max + 1) - len(b))) + b
         for i in range(1, length_max + 1):
             total = (a[-i] == '1') + (b[-i] == '1') + (car == '1')
-            if total == 0:
-                new = '0' + new
-                car = '0'
-            elif total == 1:
-                new = '1' + new
-                car = '0'
-            elif total == 2:
-                new = '0' + new
-                car = '1'
-            else:  
-                new = '1' + new
-                car = '1'
+            if total == 0:new,car = '0' + new,'0'
+            elif total == 1:new,car = '1' + new,'0'
+            elif total == 2:new,car= '0' + new,'1'
+            else:new,car = '1' + new,'1'
         if car == '1':
             new = '1' + new
         return new
