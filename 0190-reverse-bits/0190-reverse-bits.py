79d1fc94a410b2051d53ff43b0c3ffc7309815ed
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-23 23:18:24 +0530

    Time: 17 ms (58.12%), Space: 12.4 MB (49.86%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,5 +1,4 @@
 class Solution(object):
     def reverseBits(self, n):
-        temp = bin(n)[2:]
-        temp = (('0' * (32 - len(temp))) + temp)[::-1]  
-        return int(temp, 2)
+        temp = bin(n)[2:]  
+        return int((('0' * (32 - len(temp))) + temp)[::-1], 2)
