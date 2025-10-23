e68fb570206332950038466ecaeb6753ba7e23af
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-23 22:44:33 +0530

    Time: 15 ms (45.33%), Space: 17.9 MB (11.35%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,6 +2,5 @@ class Solution(object):
     def countBits(self, n):
         a = []
         for i in range(n + 1):
-            temp = bin(i)[2:]
-            a.append(temp.count('1'))
+            a.append((bin(i)[2:]).count('1'))
         return a
