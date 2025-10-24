e39f61460c21ef176726ed70267cf32c55a823b8
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-25 01:11:48 +0530

    Time: 1012 ms (5.02%), Space: 14.5 MB (34.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,9 +4,7 @@ class Solution(object):
             return nums
         if len(set(nums))==k:
             return list(set(nums))
-        dic={}
-        for i in set(nums):
-            dic[i]=nums.count(i)
+        dic = {i: nums.count(i) for i in set(nums)}
         sorted_items = sorted(dic.items(), key=lambda x: x[1], reverse=True)
         return [item[0] for item in sorted_items[:k]]
 
