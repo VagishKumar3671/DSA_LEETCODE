04ef61207f650685f7d4a7e7d75b612eeb9a732b
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-25 02:03:30 +0530

    Time: 1075 ms (5.02%), Space: 14.4 MB (41.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        if len(nums) == 1:
+        if len(nums)==1:
             return nums
-        if len(set(nums)) == k:
+        if len(set(nums))==k:
             return list(set(nums))
-        dic = {}
-        for i in nums:
-            if i in dic:
-                dic[i] += 1
-            else:
-                dic[i] = 1
+        dic={}
+        for i in set(nums):
+            dic[i]=nums.count(i)
         sorted_items = sorted(dic.items(), key=lambda x: x[1], reverse=True)
         return [item[0] for item in sorted_items[:k]]
+
