8d9f78d8c045e231ed1feb21b0113ce8a4d2e8d9
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-26 01:06:27 +0530

    Time: 11 ms (36.7%), Space: 12.5 MB (54.4%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,7 +3,6 @@ class Solution:
         dummy = ListNode(-1)
         current = dummy
         carry = 0
-
         while l1 or l2 or carry:
             if l1:
                 val1 = l1.val
@@ -13,16 +12,12 @@ class Solution:
                 val2 = l2.val
             else:
                 val2 = 0
-
             total = val1 + val2 + carry
             carry = total // 10
-
             current.next = ListNode(total % 10)
             current = current.next
-
             if l1:
                 l1 = l1.next
             if l2:
                 l2 = l2.next
-
         return dummy.next
