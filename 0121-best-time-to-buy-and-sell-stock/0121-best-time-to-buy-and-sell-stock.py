218e3b3e731e8a93713b54ceb8e66b02a0499f51
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-26 19:50:10 +0530

    Time: 150 ms (9.73%), Space: 19.5 MB (23.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,10 +2,6 @@ class Solution(object):
     def maxProfit(self, prices):
         if len(prices) == 1:
             return 0
-        if sorted(prices) == prices:
-            return prices[-1] - prices[0]
-        if sorted(prices, reverse=True) == prices:
-            return 0
         max_sum = 0
         mins = prices[0]
         for i in range(1, len(prices)):
