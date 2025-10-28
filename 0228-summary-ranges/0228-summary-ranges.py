b8fe4699a3a9f812365ec8f69b71077678a38a21
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-10-28 23:06:16 +0530

    Time: 0 ms (100%), Space: 12.6 MB (12.43%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -3,9 +3,7 @@ class Solution(object):
         res = []
         if not nums:
             return res
-
         start = nums[0]
-
         for i in range(1, len(nums)):
             if nums[i] != nums[i - 1] + 1:
                 if start == nums[i - 1]:
@@ -13,11 +11,8 @@ class Solution(object):
                 else:
                     res.append(str(start) + "->" + str(nums[i - 1]))
                 start = nums[i]
-
-        # handle the last range
         if start == nums[-1]:
             res.append(str(start))
         else:
             res.append(str(start) + "->" + str(nums[-1]))
-
         return res
