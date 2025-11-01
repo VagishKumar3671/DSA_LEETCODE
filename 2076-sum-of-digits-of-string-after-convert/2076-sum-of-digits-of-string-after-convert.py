d53bfe1332128b05bb9a0056fbe8125d6033f995
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-11-02 01:18:12 +0530

    Time: 7 ms (58.08%), Space: 12.3 MB (83.23%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -2,7 +2,7 @@ class Solution(object):
     def getLucky(self, s, k):
         temp = ''
         for i in s:
-            temp += str(ord(i) - ord('a') + 1)        
+            temp += str(ord(i)-96)        
         for _ in range(k):
             num = 0
             for j in temp:
