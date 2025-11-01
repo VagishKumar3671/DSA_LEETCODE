f7e3ce6fb5e8991973871f3b8eaa3c2ca3ab075a
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-11-02 00:45:18 +0530

    Time: 3 ms (34.41%), Space: 13.2 MB (16.08%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,5 +1,5 @@
 class Solution(object):
     def thirdMax(self, nums):
-        if len(set(nums)) < 3:
-            return sorted(set(nums), reverse=True)[0]
-        return sorted(set(nums), reverse=True)[2]
+        nums = sorted(set(nums), reverse=True)  
+        if len(nums) < 3:return nums[0]
+        return nums[2]
