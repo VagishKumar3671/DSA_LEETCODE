65c493cfbfd624af2764a8d62e4551af011ba8ec
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-11-03 00:51:56 +0530

    Time: 2 ms (79.02%), Space: 12.6 MB (38.52%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def topKFrequent(self, words, k):
+        dic = {}
+        for i in words:
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+        sort_item = sorted(dic.items(),key=lambda x: (-x[1], x[0]))
+        return [item[0] for item in sort_item[:k]]
+        
+        
