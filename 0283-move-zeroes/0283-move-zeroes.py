c2d5ef07e436fc3594f4c1e247f66a139669859b
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-11-04 23:21:56 +0530

    Time: 842 ms (9.92%), Space: 13.7 MB (36.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,7 +1,9 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        
+        if len(nums)==1:
+            return nums
+        for i in range(len(nums)):
+            if nums[i]==0:
+                nums.remove(nums[i])
+                nums.append(0)
+        return nums
