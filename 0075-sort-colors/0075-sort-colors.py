f6ae1a3883c5bd4a6c08a4f7e6ae9ee568d2b1d4
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-11-09 00:01:08 +0530

    Time: 0 ms (100%), Space: 12.5 MB (53.1%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def sortColors(self, nums):
-        c0=c1=c2=0
+        c0=c1=0
         for i in nums:
             if i == 0:
                 c0 += 1
             elif i == 1:
                 c1 += 1
-            else:
-                c2 += 1
-        nums[:] = [0]*c0 + [1]*c1 + [2]*c2
+            else:continue
+        nums[:] = [0]*c0 + [1]*c1 + [2]*(len(nums)-(c0+c1))
         return nums
