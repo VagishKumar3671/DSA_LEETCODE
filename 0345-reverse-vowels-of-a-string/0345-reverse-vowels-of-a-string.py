dc22db6bcf72a005134f382f3b8991b23b3cfc70
VAGISH KUMAR GANESH KUMAR <163675652+VagishKumar3671@users.noreply.github.com>
2025-11-09 22:54:03 +0530

    Time: 12 ms (85.09%), Space: 13.6 MB (90.89%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -3,7 +3,6 @@ class Solution(object):
         vowels = set("aeiouAEIOU")
         s = list(s)
         i, j = 0, len(s) - 1
-
         while i < j:
             if s[i] not in vowels:
                 i += 1
@@ -14,5 +13,4 @@ class Solution(object):
             s[i], s[j] = s[j], s[i]
             i += 1
             j -= 1
-        
         return "".join(s)
